6c9b920dfee45d0ed7e7dba5b8cf6bc75ead073c
yaorui <rui.yao@ele.me>
2016-07-28 20:43:30 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    sgesture: './sgesture.js',
+  },
+
+  output: {
+    path: './assets',
+    filename: '[name].min.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          plugins: ['transform-runtime', 'transform-decorators-legacy'],
+          presets: ['es2015', 'stage-0', 'react'],
+        },
+      },
+    ],
+  },
+
+  devtool: 'source-map',
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ],
+};
+
